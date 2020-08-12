3a3bdf5424846c4b1d183670bbd85f68617ab9f0
salil-jain <j.salil36@gmail.com>
2020-08-12 17:51:39 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b5370814af4d6c3c9859aa916045fc0.js"
+  "/precache-manifest.e6b60873f8507612b6a4e3344789bea8.js"
 );
 
 self.addEventListener('message', (event) => {
